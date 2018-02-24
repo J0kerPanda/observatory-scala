38ee3198b209030a0a7dc3a411bc9e65557d5750
Semenov <anton.semenov@metahouse.ru>
2018-02-24 12:50:16 +0300

    Initial commit

---
+++ src/main/scala/observatory/Main.scala
@@ -0,0 +1,18 @@
+package observatory
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Main extends App {
+
+  override def main(args: Array[String]): Unit = {
+    Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
+
+    val sc = new SparkContext("local", "Observatory", new SparkConf())
+
+    sc.textFile("src/main/resources/test.txt")
+      .collect()
+      .toList
+      .foreach(println)
+  }
+}
